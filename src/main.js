f7cbff1b71639313d862c24fcf9afc791154606a
Striker Eureka <pablocaceros@gmail.com>
2021-05-01 18:59:02 -0500

    Primer commit del proyecto

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from "vue"; // importar la libreria con npm
+import App from "./App.vue"; // importamos el componente App
+import "@/assets/css/tailwind.css";
 
 Vue.config.productionTip = false;
 
 new Vue({
   render: (h) => h(App),
-}).$mount("#app");
+}).$mount("#app"); // Mimso proposito del elemento el

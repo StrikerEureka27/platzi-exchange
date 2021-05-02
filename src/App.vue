220cdc1c5cccfae9c097638edfd7c96ecbb9dfb1
Striker Eureka <pablocaceros@gmail.com>
2021-05-02 01:03:29 -0500

    avanzando con el proyecto

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,28 @@
 <template>
-  <main id="app"></main>
+  <main id="app">
+    <px-header></px-header>
+  </main>
 </template>
 
 <script>
 
+import PxHeader from '@/components/PxHeader';
+
 export default {
   name: "App",
   components: {
-    
+    PxHeader
   },
 };
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
-}
+  margin-top: 0px;
+} */
 </style>
